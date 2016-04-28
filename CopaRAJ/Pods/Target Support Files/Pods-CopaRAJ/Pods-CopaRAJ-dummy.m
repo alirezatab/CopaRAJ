a7f4f4140f578864b8f5fadce182b45914012ceb
James Rochabrun <jamesrochabrun@gmail.com>
2016-04-28 01:01:51 -0700

    oh my god work

--- CopaRAJ/Pods/Target Support Files/Pods-CopaRAJ/Pods-CopaRAJ-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CopaRAJ : NSObject
-@end
-@implementation PodsDummy_Pods_CopaRAJ
-@end
--- CopaRAJ/Pods/Target Support Files/Pods-CopaRAJUITests/Pods-CopaRAJUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CopaRAJUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CopaRAJUITests
-@end
