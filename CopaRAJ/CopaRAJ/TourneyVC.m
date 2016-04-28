7d206ee17ca9f5c250993e45f5e96e873eb3db91
ALIREZA TABRIZI <alirezatabrizi10@gmail.com>
2016-04-28 00:29:19 -0700

    playoff updates based on the dates included in firebase but bug issue needs fixing

--- CopaRAJ/CopaRAJ/TourneyVC.m
+++ CopaRAJ/CopaRAJ/TourneyVC.m
@@ -24,10 +24,14 @@
 @property NSManagedObjectContext *moc;
 @property NSMutableArray *teams;
 @property NSMutableArray *groups;
+
 @property NSMutableArray *playoffTeams;
+@property NSArray *sortedPlayoffTeams;
+
 @property NSMutableArray *matchesData;
 @property NSMutableArray *matchesObject;
 @property NSMutableArray *playOffMatchesFromPlist;
+@property NSMutableArray *miniArray;
 
 @property FBMatch *matchA1B2;
 @property FBMatch *matchB1A2;
@@ -38,6 +42,8 @@
 @property FBMatch *matchL29L30;
 @property FBMatch *matchW29W30;
 
+@property BOOL isPlayOff;
+
 //@property Match *match;
 //@property Team *team;
 
@@ -47,44 +53,99 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    NSLog(@"view did load");
     self.navigationItem.hidesBackButton = YES;
     
     self.playoffTeams = [[NSMutableArray alloc]init];
     self.matchesObject = [[NSMutableArray alloc]init];
-
+    
+    self.matchA1B2 = [[FBMatch alloc]init];
+    self.matchB1A2 = [[FBMatch alloc]init];
+    self.matchD1C2 = [[FBMatch alloc]init];
+    self.matchC1D2 = [[FBMatch alloc]init];
+    self.matchW25W27 = [[FBMatch alloc]init];
+    self.matchW26W28 = [[FBMatch alloc]init];
+    self.matchL29L30 = [[FBMatch alloc]init];
+    self.matchW29W30 = [[FBMatch alloc]init];
+    
+    [self createDefaultPlayoffMatches];
+    
+    [self.matchesObject addObjectsFromArray:@[@[self.matchA1B2, self.matchB1A2, self.matchD1C2, self.matchC1D2], @[self.matchW25W27, self.matchW26W28], @[self.matchL29L30, self.matchW29W30], @[]]];
+    
+    NSLog(@"%lu", self.matchesObject.count);
+    
     // URL that fire base accesses
-    Firebase *ref = [[Firebase alloc]initWithUrl:@"https://fiery-inferno-5799.firebaseio.com/matches"];
+//    Firebase *ref = [[Firebase alloc]initWithUrl:@"https://fiery-inferno-5799.firebaseio.com/matches"];
+    Firebase *ref = [[Firebase alloc]initWithUrl:@"https://fiery-inferno-5799.firebaseio.com/tests/matches"];
     // FireBase Listener
-    [ref observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-        
-        NSDictionary *match1Dictionary = [snapshot.value objectForKey:@"347342"];
-        NSDictionary *match2Dictionary = [snapshot.value objectForKey:@"347343"];
-        NSDictionary *match3Dictionary = [snapshot.value objectForKey:@"347344"];
-        NSDictionary *match4Dictionary = [snapshot.value objectForKey:@"347345"];
-        NSDictionary *match5Dictionary = [snapshot.value objectForKey:@"347346"];
-        NSDictionary *match6Dictionary = [snapshot.value objectForKey:@"347347"];
-        NSDictionary *match7Dictionary = [snapshot.value objectForKey:@"347348"];
-        NSDictionary *match8Dictionary = [snapshot.value objectForKey:@"347349"];
-        
-        //NSLog(@"Firebase match data: %@", matchDictionary);
-        
-        self.matchA1B2 = [self createPlayOffMatch:match1Dictionary];
-        self.matchB1A2 = [self createPlayOffMatch:match2Dictionary];
-        self.matchD1C2 = [self createPlayOffMatch:match3Dictionary];
-        self.matchC1D2 = [self createPlayOffMatch:match4Dictionary];
-        self.matchW25W27 = [self createPlayOffMatch:match5Dictionary];
-        self.matchW26W28 = [self createPlayOffMatch:match6Dictionary];
-        self.matchL29L30 = [self createPlayOffMatch:match7Dictionary];
-        self.matchW29W30 = [self createPlayOffMatch:match8Dictionary];
-        
-        [self.matchesObject addObjectsFromArray:@[self.matchA1B2, self.matchB1A2, self.matchD1C2, self.matchC1D2, self.matchW25W27, self.matchW26W28, self.matchL29L30, self.matchW29W30]];
-        
-        [self.collectionView reloadData];
+    [ref observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
+//    [ref observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        NSNumber *isPlayoffs = snapshot.value[@"playoffs"];
+        self.isPlayOff = isPlayoffs.boolValue;
+        NSLog(@"%d", self.isPlayOff);
+        NSMutableArray *playoffTeams = [NSMutableArray new];
+        if (self.isPlayOff == 1) {
+            if ([self.matchA1B2.schedule isEqualToString: snapshot.value[@"schedule"]]) {
+                self.matchA1B2.matchID = snapshot.value[@"id"];
+                
+                self.matchA1B2.local = snapshot.value[@"local"];
+                self.matchA1B2.local_abbr = snapshot.value[@"local_abbr"];
+                self.matchA1B2.local_goals = snapshot.value[@"local_goals"];
+                
+                self.matchA1B2.visitor = snapshot.value[@"visitor"];
+                self.matchA1B2.visitor_abbr = snapshot.value[@"visitor_abbr"];
+                self.matchA1B2.visitor_goals = snapshot.value[@"visitor_goals"];
+                
+                [self.collectionView reloadData];
+            } else if ([self.matchB1A2.schedule isEqualToString: snapshot.value[@"schedule"]]){
+                self.matchB1A2.matchID = snapshot.value[@"id"];
+                
+                self.matchB1A2.local = snapshot.value[@"local"];
+                self.matchB1A2.local_abbr = snapshot.value[@"local_abbr"];
+                self.matchB1A2.local_goals = snapshot.value[@"local_goals"];
+                
+                self.matchB1A2.visitor = snapshot.value[@"visitor"];
+                self.matchB1A2.visitor_abbr = snapshot.value[@"visitor_abbr"];
+                self.matchB1A2.visitor_goals = snapshot.value[@"visitor_goals"];
+                
+                [self.collectionView reloadData];
+            } else if ([self.matchD1C2.schedule isEqualToString: snapshot.value[@"schedule"]]){
+                self.matchD1C2.matchID = snapshot.value[@"id"];
+                
+                self.matchD1C2.local = snapshot.value[@"local"];
+                self.matchD1C2.local_abbr = snapshot.value[@"local_abbr"];
+                self.matchD1C2.local_goals = snapshot.value[@"local_goals"];
+                
+                self.matchD1C2.visitor = snapshot.value[@"visitor"];
+                self.matchD1C2.visitor_abbr = snapshot.value[@"visitor_abbr"];
+                self.matchD1C2.visitor_goals = snapshot.value[@"visitor_goals"];
+                
+                [self.collectionView reloadData];
+            } else if ([self.matchC1D2.schedule isEqualToString: snapshot.value[@"schedule"]]){
+                
+                
+                self.matchC1D2.local = snapshot.value[@"local"];
+                
+                self.matchC1D2.visitor = snapshot.value[@"visitor"];
+                
+                [self.collectionView reloadData];
+            } else if ([self.matchW25W27.schedule isEqualToString: snapshot.value[@"schedule"]]){
+                //NSLog(@"Match ID: %@\n is at: %@", snapshot.value[@"id"], snapshot.value[@"schedule"]);
+            } else if ([self.matchW26W28.schedule isEqualToString: snapshot.value[@"schedule"]]){
+                //NSLog(@"Match ID: %@\n is at: %@", snapshot.value[@"id"], snapshot.value[@"schedule"]);
+            } else if ([self.matchW29W30.schedule isEqualToString: snapshot.value[@"schedule"]]){
+                //NSLog(@"Match ID: %@\n is at: %@", snapshot.value[@"id"], snapshot.value[@"schedule"]);
+            }
+            NSLog(@"%@", snapshot.value[@"id"]);
+            [playoffTeams addObject:snapshot.value[@"id"]];
+            NSLog(@"%lu", (unsigned long)self.playoffTeams.count);
+        }
         
     } withCancelBlock:^(NSError *error) {
         NSLog(@"%@", error.description);
     }];
+
+//    NSLog(@"%lu", self.matchesObject.count);
 }
 
 #pragma mark - CollectionView
@@ -98,26 +159,21 @@
 
     switch (indexPath.section) {
         case 0:{
-            FBMatch *cellMatch1 = self.matchA1B2;
-            FBMatch *cellMatch2 = self.matchB1A2;
-            FBMatch *cellMatch3 = self.matchD1C2;
-            FBMatch *cellMatch4 = self.matchC1D2;
-            
-            cell.FBMatch = cellMatch1;
-            cell.FBMatch = cellMatch2;
-            cell.FBMatch = cellMatch3;
-            cell.FBMatch = cellMatch4;
+            self.miniArray = [[NSMutableArray alloc]init];
+            self.miniArray = [self.matchesObject objectAtIndex:0];
+            cell.FBMatch = [self.miniArray objectAtIndex:indexPath.row];
             return cell;
         }
         case 1:{
-            FBMatch *cellMatch5 = self.matchW25W27;
-            FBMatch *cellMatch6 = self.matchW26W28;
-            cell.FBMatch = cellMatch5;
-            cell.FBMatch = cellMatch6;
+            self.miniArray = [[NSMutableArray alloc]init];
+            self.miniArray = [self.matchesObject objectAtIndex:1];
+            cell.FBMatch = [self.miniArray objectAtIndex:indexPath.row];
             return cell;
         }
         case 2:{
-            FBMatch *cellMatch = [self.matchesObject objectAtIndex:indexPath.row+7];
+            self.miniArray = [[NSMutableArray alloc]init];
+            self.miniArray = [self.matchesObject objectAtIndex:2];
+            FBMatch *cellMatch = [self.miniArray objectAtIndex:indexPath.row+1];
             cell.FBMatch = cellMatch;
             return cell;
         }
@@ -128,7 +184,6 @@
         default:
             break;
     }
-    
     return cell;
 }
 
@@ -144,8 +199,7 @@
     }
 }
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
-    return self.matchesObject.count/2;
-    
+    return self.matchesObject.count;
 }
 -(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
     return CGSizeMake(self.view.frame.size.width/1.5, ((self.view.frame.size.height - 60)/5));
@@ -173,42 +227,81 @@
 
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
     NSLog(@"index path: %@", indexPath);
+    NSLog(@"%@", self.matchA1B2.matchID);
    // [self performSegueWithIdentifier:@"TournamentToDetail" sender:self];
 }
-
-
--(FBMatch *) createPlayOffMatch:(NSDictionary *)dictionary{
-    FBMatch *matchObject = [FBMatch new];
-    
-    matchObject.matchID = dictionary[@"id"];
-    
-    matchObject.local = dictionary[@"local"];
-    //NSLog(@"local Team: %@", matchData1.local);
-    matchObject.local_abbr = dictionary[@"local_abbr"];
-    //NSLog(@"local Team_abbr: %@", matchObject.local_abbr);
-    matchObject.local_goals = dictionary[@"local_goals"];
-    //NSLog(@"local Team goals: %@", matchData1.local_goals);
-    matchObject.pen1 = dictionary[@"pen1"];
-    //NSLog(@"local Team penalty: %@", matchData1.pen1);
-    
-    matchObject.visitor = dictionary[@"visitor"];
-    //NSLog(@"visitor Team: %@", matchData1.visitor);
-    matchObject.visitor_abbr = dictionary[@"visitor_abbr"];
-    //NSLog(@"visitor Team_abbr: %@", matchData1.visitor_abbr);
-    matchObject.visitor_goals = dictionary[@"visitor_goals"];
-    //NSLog(@"visitor Team goals: %@", matchData1.visitor_goals);
-    matchObject.pen2 = dictionary[@"pen2"];
-    //NSLog(@"visitor Team penalty: %@", matchData1.pen2);
-    
-    return matchObject;
+     
+-(void) createDefaultPlayoffMatches{
+         Firebase *ref = [[Firebase alloc]initWithUrl:@"https://fiery-inferno-5799.firebaseio.com/Playoff_Schedule"];
+         // FireBase Listener
+         [ref observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        
+             self.matchA1B2.local = @"A1";
+             self.matchA1B2.visitor = @"B2";
+             NSLog(@"%@", snapshot.value);
+             self.matchA1B2.schedule = [snapshot.value objectForKey:@"A1B2"];
+             
+             self.matchB1A2.local = @"B1";
+             self.matchB1A2.visitor = @"A2";
+             self.matchB1A2.schedule = [snapshot.value objectForKey:@"B1A2"];
+             
+             self.matchD1C2.local = @"D1";
+             self.matchD1C2.visitor = @"C2";
+             self.matchD1C2.schedule = [snapshot.value objectForKey:@"D1C2"];
+              
+             self.matchC1D2.local = @"C1";
+             self.matchC1D2.visitor = @"D2";
+             self.matchC1D2.schedule = [snapshot.value objectForKey:@"C1D2"];
+              
+             self.matchW25W27.local = @"W25";
+             self.matchW25W27.visitor = @"W27";
+             self.matchW25W27.schedule = [snapshot.value objectForKey:@"W25W27"];
+              
+             self.matchW26W28.local = @"W26";
+             self.matchW26W28.visitor = @"W28";
+             self.matchW26W28.schedule = [snapshot.value objectForKey:@"W26W28"];
+              
+             self.matchW29W30.local = @"W290";
+             self.matchW29W30.visitor = @"W30";
+             self.matchW29W30.schedule = [snapshot.value objectForKey:@"W29W30"];
+             
+             //not best way cause of the lag
+             [self.collectionView reloadData];
+         } withCancelBlock:^(NSError *error) {
+              NSLog(@"%@", error.description);
+         }];
 }
-
+//-(FBMatch *) createPlayOffMatch:(NSDictionary *)dictionary{
+//    FBMatch *matchObject = [FBMatch new];
+//    
+//    matchObject.matchID = dictionary[@"id"];
+//    //NSLog(@"local Team: %@", matchObject.matchID);
+//    matchObject.local = dictionary[@"local"];
+//    //NSLog(@"local Team: %@", matchObject.local);
+//    matchObject.local_abbr = dictionary[@"local_abbr"];
+//    //NSLog(@"local Team_abbr: %@", matchObject.local_abbr);
+//    matchObject.local_goals = dictionary[@"local_goals"];
+//    //NSLog(@"local Team goals: %@", matchObject.local_goals);
+//    matchObject.pen1 = dictionary[@"pen1"];
+//    //NSLog(@"local Team penalty: %@", matchObject.pen1);
+//    
+//    matchObject.visitor = dictionary[@"visitor"];
+//    //NSLog(@"visitor Team: %@", matchObject.visitor);
+//    matchObject.visitor_abbr = dictionary[@"visitor_abbr"];
+//    //NSLog(@"visitor Team_abbr: %@", matchObject.visitor_abbr);
+//    matchObject.visitor_goals = dictionary[@"visitor_goals"];
+//    //NSLog(@"visitor Team goals: %@", matchObject.visitor_goals);
+//    matchObject.pen2 = dictionary[@"pen2"];
+//    //NSLog(@"visitor Team penalty: %@", matchObject.pen2);
+//    
+//    return matchObject;
+//}
 #pragma mark - MemoryWarning
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+#pragma mark - Segue
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
 //    GameVC *desVC = segue.destinationViewController;
 //    NSArray *indexPaths = [self.collectionView indexPathsForSelectedItems];
@@ -219,130 +312,3 @@
 //    NSLog(@"%@", cellMatch);
 }
 @end
-
-//    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
-//    self.moc = appDelegate.managedObjectContext;
-//
-//    [self pullTeamsFromCoreData];
-//    [self pullGroupsFromCoreData];
-
-//    for (Group *group in self.groups) {
-//        [self conductJsonSearchForGroup:group];
-//    }
-//NSLog(@"sqlite dir = \n%@", appDelegate.applicationDocumentsDirectory);
-
-
-//#pragma mark - Core Data
-//- (void)pullTeamsFromCoreData {
-//    NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"Team"];
-//    NSError *error;
-//    NSMutableArray *coreDataArray = [[self.moc executeFetchRequest:request error:&error]mutableCopy];
-//
-//    if (error == nil) {
-//        self.teams = [[NSMutableArray alloc]initWithArray:coreDataArray];
-//    } else {
-//        NSLog(@"%@", error);
-//    }
-//
-//    if (self.teams.count == 0) {
-//        NSLog(@"Core data doesn't have any teams");
-//    }
-//}
-//
-//- (void)pullGroupsFromCoreData {
-//
-//    NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"Group"];
-//    NSError *error;
-//    NSMutableArray *coreDataArray = [[self.moc executeFetchRequest:request error:&error]mutableCopy];
-//
-//    if (error == nil) {
-//        self.groups = [[NSMutableArray alloc]initWithArray:coreDataArray];
-//    } else {
-//        NSLog(@"%@", error);
-//    }
-//
-//    if (self.groups.count == 0) {
-//        NSLog(@"Core data doesn't have any Groups");
-//    }
-//}
-//
-//- (void) conductJsonSearchForGroup: (Group *)group {
-//
-//    NSMutableArray *groupTeams = [NSMutableArray new];
-//    for (Team *team in group.teams) {
-//        [groupTeams addObject:team];
-//    }
-//
-//    NSString *searchVariable = [self returnGroupNameAsNumberForSearchFromName:group.groupID];
-//
-//    NSString *urlString = [NSString stringWithFormat:@"http://www.resultados-futbol.com/scripts/api/api.php?key=40b2f1fd2a56cbd88df8b2c9b291760f&req=tables&format=json&tz=America/Chicago&lang=en&league=177&group=%@&year=2015", searchVariable];
-//    NSURL *url = [NSURL URLWithString: urlString];
-//    NSURLSession *session = [NSURLSession sharedSession];
-//
-//    NSURLSessionTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-//        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-//        NSMutableArray *table = dictionary[@"table"];
-//
-//        for (NSDictionary *team in table) {
-//            //NSLog(@"The team from  Json is %@", team[@"team"]);
-//            [self updateTeamFromTeamArray:groupTeams WithLatestDictionary:team];
-//            //[self updateMatchsArray: <your match array> withDictionary:<jsonDictionaryforIndividualMatch>]
-//        }
-////        NSError *saveError;
-////        if ([self.moc save:&saveError]) {
-////            NSLog(@"Teams updated");
-////            [self.tableView reloadData];
-////        } else {
-////            NSLog(@"Team updates resulted in the following error: %@", saveError);
-////        }
-//    }];
-//    [task resume];
-//}
-//
-//-(NSString *)returnGroupNameAsNumberForSearchFromName: (NSString *)groupName{
-//
-//    NSString *groupNumber = @"";
-//
-//    if ([groupName  isEqualToString: @"A"]) {
-//        groupNumber = @"1";
-//    } else if ([groupName  isEqualToString: @"B"]){
-//        groupNumber = @"2";
-//    } else {
-//        groupNumber = @"3";
-//    }
-//    return groupNumber;
-//}
-//
-//- (void)updateTeamFromTeamArray:(NSMutableArray *)teams WithLatestDictionary:(NSDictionary *)dictionary {
-//
-//    NSString *teamNameFromDictionary = dictionary[@"team"];
-//    Team *teamForDictionary;
-//
-//    for (Team *team in teams) {
-//        if ([team.countryName isEqualToString:teamNameFromDictionary]) {
-//            teamForDictionary = team;
-//        }
-//    }
-//    teamForDictionary.wins = dictionary[@"wins"];
-//    teamForDictionary.points = dictionary[@"points"];
-//    teamForDictionary.losses = dictionary[@"losses"];
-//    teamForDictionary.id = dictionary[@"id"];
-//    teamForDictionary.goalsFor = dictionary[@"gf"];
-//    teamForDictionary.goalsAgainst = dictionary[@"ga"];
-//    teamForDictionary.gamesPlayed = dictionary[@"round"];
-//    teamForDictionary.position = dictionary[@"pos"];
-//    teamForDictionary.draws = dictionary[@"draws"];
-//
-//    //NSLog(@"The team that will be updated is %@", teamForDictionary.countryName);
-//}
-//- (void)testLabelMatches:(int)index withArrayIndex:(int)newindex{
-//    Group *group = [self.groups objectAtIndex:index];
-//    NSLog(@"this is the new  %@", group);
-//    NSArray *sortedArray = [group returnGroupTeamsOrderedByPointsForGroup:group];
-//    Team *team = [sortedArray objectAtIndex:newindex];
-//    NSLog(@"this is the team %@", team.countryName);
-//    [self.playoffTeams addObject:team];
-//    NSLog(@"%@", team.abbreviationName);
-//}
-
-
