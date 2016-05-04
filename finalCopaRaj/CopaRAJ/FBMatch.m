2004ae65eb540bd4bc261972d4b7d46fca468078
James Rochabrun <jamesrochabrun@gmail.com>
2016-05-04 11:21:56 -0700

    last test

--- finalCopaRaj/CopaRAJ/FBMatch.m
+++ finalCopaRaj/CopaRAJ/FBMatch.m
@@ -15,6 +15,16 @@
     for (NSDictionary *goal in match.goals) {
         [nonsortedArray addObject:goal];
     }
+    
+    //kickoff
+    NSDictionary *kickOff = @{
+                              @"action" : @"kickoff",
+                              @"minute" : @"0"
+                              };
+    
+    [nonsortedArray addObject:kickOff];
+    
+    
     //cards
     for (NSDictionary *cards in match.cards) {
         [nonsortedArray addObject:cards];
@@ -255,17 +265,11 @@
         }
         
         //timeline
-      NSLog(@"count of goals %lu", (unsigned long)match.goals.count);
-        if (match.goals.count <1 && match.cards.count < 1 && match.changes.count < 1) {
-        } else {
+     
             NSArray *timeLine = [FBMatch createTimeLineWithMatch: match];
             if (timeLine.count > 0) {
                 match.timeline = [NSMutableArray arrayWithArray:[NSArray arrayWithArray:timeLine]];
             }
-        }
-      
-      if ([match.status isEqualToString:@"-1"]) {
-      }
     }//if there is extra data closing bracket
     //NSLog(@"%@", match);
     //NSLog(@"%@ is the status", match.status);
