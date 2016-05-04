8ee3bf5d2cf991f41ff90b0a1dee8b7df6ef8ec8
James Rochabrun <jamesrochabrun@gmail.com>
2016-05-03 21:49:14 -0700

    last commit

--- finalCopaRaj/CopaRAJ/GameVC.m
+++ finalCopaRaj/CopaRAJ/GameVC.m
@@ -56,7 +56,7 @@
 }
 
 - (void)eventsImageAppear {
-    self.tableView.hidden = YES;
+    self.tableView.hidden = NO;
 }
 
 - (void)updateCounter:(NSTimer *)tmr
@@ -87,7 +87,8 @@
 
 - (void)listenToMatch {
     
-    NSString *url = [NSString stringWithFormat:@"https://fiery-inferno-5799.firebaseio.com/matches/%@",self.match.matchID];
+    NSString *url = [NSString stringWithFormat:@"https://fiery-inferno-5799.firebaseio.com/matches/377719"];
+                     //self.match.matchID];
                      //self.match.matchID];
     Firebase *ref = [[Firebase alloc]initWithUrl:url];
     [ref observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
@@ -114,11 +115,18 @@
     self.title = titleText;
   
   if ([self.match.status isEqualToString: @"-1"]) {
-    self.timeLabel.text = [NSString stringWithFormat:@"%@ : %@", self.match.hour , self.match.minute];
+      
+      NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+      dateFormatter.dateFormat = @"HH:mm";
+      NSDate *date = [dateFormatter dateFromString:[NSString stringWithFormat:@"%@:%@", self.match.hour, self.match.minute]];
+      dateFormatter.dateFormat = @"hh:mm a";
+      NSString *dateString = [dateFormatter stringFromDate:date];
+      
+    self.timeLabel.text = [NSString stringWithFormat:@"%@ ET" ,dateString];
     self.teamOneScore.text = @"";
     self.teamTwoScore.text = @"";
   } else if ([self.match.status isEqualToString:@"0"]){
-    self.timeLabel.text = self.match.live_minute;
+    self.timeLabel.text = [NSString stringWithFormat:@"%@'", self.match.live_minute];
     self.teamOneScore.text = self.match.local_goals;
     self.teamTwoScore.text = self.match.visitor_goals;
   } else if ([self.match.status isEqualToString:@"1"])  {
@@ -127,16 +135,20 @@
     self.teamTwoScore.text = self.match.visitor_goals;
 
   }
-    self.teamOneImage.image = [UIImage imageNamed:@"Colombia"];//[UIImage imageNamed:self.match.local];
+    self.teamOneImage.image = [UIImage imageNamed:self.match.local];
     self.teamOneName.text = self.match.local_abbr;
-    self.teamTwoImage.image = [UIImage imageNamed:@"Peru"];//[UIImage imageNamed:self.match.visitor];
+    self.teamTwoImage.image = [UIImage imageNamed:self.match.visitor];
     self.teamTwoName.text = self.match.visitor_abbr;
     self.matchDateLabel.text = self.match.date;
-    NSArray *labels = @[self.timeLabel , self.teamOneName , self.teamOneScore , self.teamTwoName ,self.teamTwoScore , self.matchDateLabel , self.versusLabel , self.locationLabel];
+    NSArray *labels = @[self.timeLabel , self.teamOneName ,self.teamTwoName, self.matchDateLabel , self.versusLabel , self.locationLabel];
     for (UILabel *label in labels){
         [label setFont:[UIFont fontWithName:@"GOTHAM MEDIUM" size:16]];
         [label setTextColor:[UIColor whiteColor]];
     }
+    [self.teamOneScore setFont:[UIFont fontWithName:@"GOTHAM MEDIUM" size:30]];
+    [self.teamOneScore setTextColor: [UIColor whiteColor]];
+    [self.teamTwoScore setFont:[UIFont fontWithName:@"GOTHAM MEDIUM" size:30]];
+    [self.teamTwoScore setTextColor:[UIColor whiteColor]];
 }
 
 - (void)displayLineUpLabels {
@@ -146,7 +158,7 @@
     int i = 0;
     NSArray *lineUpLocalLabels = @[self.teamAPlayer1,self.teamAPlayer2,self.teamAPlayer3,self.teamAPlayer4,self.teamAPlayer5,self.teamAPlayer6,self.teamAPlayer7,self.teamAPlayer8,self.teamAPlayer9,self.teamAPlayer10,self.teamAPlayer11];
     
-    self.lineUpLocalFlag.image = [UIImage imageNamed:@"Argentina"];/////[UIImage imageNamed:self.match.local];
+    self.lineUpLocalFlag.image =[UIImage imageNamed:self.match.local];
     
     if (self.match.local_Lineup) {
         for (FBMatch *player in self.match.local_Lineup) {
@@ -169,7 +181,7 @@
     
     NSArray *lineUpVisitLabels = @[self.teamBPLayer1,self.teamBPlayer2,self.teamBPlayer3,self.teamBPlayer4,self.teamBPlayer5,self.teamBPlayer6,self.teamBPlayer7,self.teamBPlayer8,self.teamBPlayer9,self.teamBPlayer10,self.teamBPlayer11];
     
-    self.lineUpVisitorFlag.image = [UIImage imageNamed:@"Bolivia"];//[UIImage imageNamed:self.match.visitor];
+    self.lineUpVisitorFlag.image = [UIImage imageNamed:self.match.visitor];
     
     if (self.match.visitor_Lineup) {
         for (FBMatch *match in self.match.visitor_Lineup) {
@@ -192,29 +204,41 @@
 
 - (void) displayStatsLabels {
     
-        //local Team A stats
+    //local Team A stats
     self.teamAStatsFlag.image = [UIImage imageNamed:self.match.local];
-    self.local_posLabel.text = self.match.local_pos;
+    if(self.match.local_pos != NULL){
+    self.local_posLabel.text = [NSString stringWithFormat:@"%@ %%" , self.match.local_pos];
+    }
     self.local_sotLabel.text = self.match.local_sot;
     self.local_sonLabel.text = self.match.local_son;
-    self.local_soffLabel.text = [NSString stringWithFormat:@"%@" , self.match.local_off];
+    if (self.match.local_off != NULL){
+        self.local_soffLabel.text = [NSString stringWithFormat:@"%@" , self.match.local_off];
+    }
     self.local_frkLabel.text = self.match.local_frk;
     self.local_blkLabel.text = self.match.local_blk;
     self.local_ycLabel.text = self.match.local_yc;
-    self.local_rcLabel.text = [NSString stringWithFormat:@"%@" , self.match.local_rc];
     self.local_cor.text = self.match.local_cor;
-  
+    if (self.match.local_rc != NULL){
+        self.local_rcLabel.text = [NSString stringWithFormat:@"%@", self.match.local_rc];
+    }
+    
     //local team B stats
     self.teamBStatsFlag.image = [UIImage imageNamed:self.match.visitor];
-    self.visitor_posLabel.text = self.match.visitor_pos;
+    if(self.match.visitor_pos != NULL){
+    self.visitor_posLabel.text = [NSString stringWithFormat: @"%@ %%",self.match.visitor_pos];
+    }
     self.visitor_sotLabel.text = self.match.visitor_sot;
     self.visitor_sonLabel.text = self.match.visitor_son;
-    self.visitor_soffLabel.text =[NSString stringWithFormat:@"%@" , self.match.visitor_off];
+    if (self.match.visitor_off != NULL){
+        self.visitor_soffLabel.text =[NSString stringWithFormat:@"%@" , self.match.visitor_off];
+    }
     self.visitor_frkLabel.text = self.match.visitor_frk;
     self.visitor_blkSaves.text = self.match.visitor_blk;
     self.visitor_ycLabel.text = self.match.visitor_yc;
-    self.visitor_rcLabel.text = [NSString stringWithFormat:@"%@", self.match.visitor_rc];
     self.visitor_cor.text = self.match.visitor_cor;
+    if (self.match.visitor_rc != NULL){
+        self.visitor_rcLabel.text = [NSString stringWithFormat:@"%@", self.match.visitor_rc];
+    }
     
     //adding font color and size to the stats labels
     NSArray *labels = @[self.local_posLabel , self.local_sotLabel , self.local_sonLabel , self.local_soffLabel , self.local_frkLabel , self.local_blkLabel , self.local_ycLabel , self.local_rcLabel , self.visitor_posLabel , self.visitor_sotLabel , self.visitor_sonLabel , self.visitor_soffLabel , self.local_frkLabel , self.visitor_frkLabel , self.visitor_blkSaves , self.visitor_ycLabel , self.visitor_rcLabel , self.posessionLabel , self.shotsLabel, self.shotsTargetLabel , self.offSideLabel , self.freeKickLabel , self.savesLabel ,self.yellowCardLabel ,self.redCardLabel , self.cornerKick , self.local_cor , self.visitor_cor];
@@ -242,16 +266,20 @@
     }
     
     BOOL b = [event objectForKey:@"player"];
-
+    
     if(b){
         cell.playerLabel.text = [event valueForKey:@"player"];
     } else {
-        cell.playerLabel.text = @"";
+        cell.playerLabel.text = @"replaced By";
     }
-
+    
     cell.actionImage.image = [UIImage imageNamed:[event valueForKey:@"action"]];
-    cell.inLabel.text = [event valueForKey:@"playerIn"];
-    cell.outLabel.text = [event valueForKey:@"playerOut"];
+    cell.inLabel.text = [event valueForKey:@"playerOut"];
+    cell.outLabel.text = [event valueForKey:@"playerIn"];
+    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, cell.contentView.frame.size.height - 1.0, cell.contentView.frame.size.width, 1)];
+    lineView.backgroundColor = [UIColor colorWithWhite:0.333 alpha:0.291]
+    ;
+    [cell.contentView addSubview:lineView];
     return cell;
 }
 
