f00d42aba662ad7f22ae54367e786c050e1f65d8
ALIREZA TABRIZI <alirezatabrizi10@gmail.com>
2016-05-19 01:13:14 -0700

    forget password is almost done. rest password TODO

--- finalCopaRaj/CopaRAJ/ChallengeLogInVC.swift
+++ finalCopaRaj/CopaRAJ/ChallengeLogInVC.swift
@@ -32,7 +32,6 @@ class ChallengeLogInVC: UIViewController, FBSDKLoginButtonDelegate, UINavigation
         
         if (NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil && DataService.dataService.CURRENT_USER_REF.authData != nil) {
             self.performSegueWithIdentifier(self.loggedIn, sender: nil)
-        
         }
         if FBSDKAccessToken.currentAccessToken() == nil {
             print("Not logged in..")
@@ -99,23 +98,65 @@ class ChallengeLogInVC: UIViewController, FBSDKLoginButtonDelegate, UINavigation
             DataService.dataService.BASE_REF.authUser(email, password: password, withCompletionBlock: { error, authData in
                 if error != nil {
                     print(error.localizedDescription);
-                    self.loginErrorAlert("Oops", message: error.localizedDescription)
+                    
+                    self.displayErrorAlert("Oops", message: error.localizedDescription)
                     
                 } else {
-                    // Be sure the correct uid is stored.
+                    // Be sure the correct uid is stored
                     NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: "uid")
+                    
+                    if ((DataService.dataService.CURRENT_USER_REF.authData.providerData["isTemporaryPassword"]?.boolValue)! == true){
+                        print("Temp Pass was used")
+                        self.displayChangePasswordAlert("Password Setup", message: "Please enter your new password", email: email!)
+                    } else {
+                        print("regular pass was used")
+                    }
                     
                     // Enter the app!
                     self.performSegueWithIdentifier(self.loggedIn, sender: nil)
                 }
             })
         } else {
-            loginErrorAlert("Oops", message: "Dont foget to enter your email")
+            displayErrorAlert("Oops", message: "Dont foget to enter your email")
         }
     }
+    
+    @IBAction func forgottenPassword(sender: AnyObject) {
         
-    func loginErrorAlert(title: String, message: String) {
+        var loginTextField: UITextField?
+        let alertController = UIAlertController(title: "Password Recovery", message: "Please enter your email address", preferredStyle: .Alert)
+        let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            
+            if loginTextField?.text != "" {
+                DataService.dataService.BASE_REF.resetPasswordForUser(loginTextField?.text, withCompletionBlock: { (error) in
+                    if (error == nil) {
+                        
+                        self.displayErrorAlert("Password reset", message: "Check your inbox to reset your password")
+                        
+                    } else {
+                        print(error)
+                        self.displayErrorAlert("Unidentified email address", message:  "Please re-enter the email you registered with")
+                    }
+                })
+            }
+            print("textfield is empty")
+            
+        })
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
+            
+        }
+        alertController.addAction(ok)
+        alertController.addAction(cancel)
+        alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            // Enter the textfiled customization code here.
+            loginTextField = textField
+            loginTextField?.placeholder = "Enter your login ID"
+        }
+        presentViewController(alertController, animated: true, completion: nil)
         
+    }
+    
+    func displayErrorAlert(title: String, message: String) {
         // Called upon login error to let the user know login didn't work.
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
         let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
@@ -123,6 +164,41 @@ class ChallengeLogInVC: UIViewController, FBSDKLoginButtonDelegate, UINavigation
         presentViewController(alert, animated: true, completion: nil)
     }
     
+    func displayChangePasswordAlert(title: String, message: String, email: String) {
+        var newPasswordTextField: UITextField?
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            
+            if newPasswordTextField?.text != "" {
+                print(DataService.dataService.CURRENT_USER_REF.authData.auth["provider"])
+                //DataService.dataService.CURRENT_USER_REF.setV
+                DataService.dataService.BASE_REF.changePasswordForUser(email, fromOld: DataService.dataService.CURRENT_USER_REF.authData.provider , toNew: newPasswordTextField?.text, withCompletionBlock: { (error) in
+                    if (error == nil) {
+                        self.displayErrorAlert("New Password", message: "Pasword has been changed")
+                    } else {
+                        print(error)
+                        //self.displayErrorAlert("Unidentified email address", message:  "Please re-enter the email you registered with")
+                    }
+                })
+            }
+            print("textfield is empty")
+            
+        })
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
+            
+        }
+        alertController.addAction(ok)
+        alertController.addAction(cancel)
+        alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            // Enter the textfiled customization code here.
+            newPasswordTextField = textField
+            newPasswordTextField?.placeholder = "Enter new password"
+        }
+        presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+    
+
     @IBAction func unwindToLogin(segue: UIStoryboardSegue) {
     }
         
